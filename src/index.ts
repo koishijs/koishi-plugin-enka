3abba35086fe541a4573fae991edfd7a4923d77a
Lipraty <sis1943@outlook.com>
2023-03-31 01:57:31 +0800

    chore: remove `cache` chore: support multiple source feat: experimental support for in-page proxies feat: support chinese character list

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import { Argv, Context, Schema, h } from 'koishi';
 import { } from 'koishi-plugin-puppeteer';
 import { } from '@koishijs/cache';
 import type { HTTPResponse, Page } from 'puppeteer-core';
+import useProxy from 'puppeteer-page-proxy';
 import map from './map.json';
 import localeMap from './locales/localeMap.json';
 
-const UUID = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
+const UUIDRegExp = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
 
 declare module 'koishi' {
     interface User {
@@ -28,14 +29,31 @@ export const name = 'enka'
 
 export const using = ['puppeteer']
 
+type AgentConfig = 'value' | 'miaoToken' | 'token'
+
 export interface Config {
-    maxAge: number
-    // reverseProxy: string
+    agent: string | Record<AgentConfig, any>
+    proxy: boolean | string
 }
 
 export const Config: Schema<Config> = Schema.object({
-    maxAge: Schema.number().min(0).max(9000000).default(0).step(1).description('cache 最大时间（毫秒，低于 30000 时为不缓存）'),
-    // reverseProxy: Schema.string().role('link').description('加速代理地址（不是梯子）')
+    agent: Schema.union([
+        Schema.const('https://enka.network').description('Default(Enka)'),
+        Schema.const('https://enka.network').description('占个位'),
+        // Schema.object({
+        //     value: Schema.const('https://api.nyan.zone/v1/genshin/player'),
+        //     token: Schema.string().description('<a href="https://paimon-display.app.lonay.me">Luna.Y Token</a>').required()
+        // }).description('Lipraty'),
+        // Schema.object({
+        //     value: Schema.const('http://miaoapi.cn'),
+        //     miaoToken: Schema.string().description('喵喵API Token').required()
+        // }).description('MiaoApi （未实装）'),
+    ]).default('https://enka.network').description('请求地址') as Schema<string | Record<AgentConfig, any>>,
+    proxy: Schema.union([
+        Schema.const(false).description('Disable'),
+        Schema.const(true).description('Root'),
+        Schema.string().role('link').description('Self'),
+    ]).description('代理设置')
 })
 
 Argv.createDomain('UID', source => {
@@ -53,9 +71,6 @@ export function apply(ctx: Context, config: Config) {
     let page: Page;
     let lock: Promise<void>;
     let mapIndex: Record<string, string> = {};
-    let cache;
-
-    ctx.using(['cache'], () => { cache = ctx.cache('enka') })
 
     ctx.command('enka [search:string]')
         .alias('原')
@@ -74,20 +89,15 @@ export function apply(ctx: Context, config: Config) {
             if (!session.user.genshin_uid) return session.text('.bind');
             if (search && !mapIndex[search]) return session.text('.non-existent');
             else search = mapIndex[search];
-
             await session.send(session.text('.relax'));
-            const cacheKey = `enka_u${session.user.genshin_uid}_${search}`;
-            //擦车
-            if (cache && config.maxAge >= 30000) {
-                const cacheValue = await cache.get(cacheKey);
-                if (cacheValue) return h.image(Buffer.from(cacheValue, 'base64').buffer, 'image/png');
-            }
             if (!page) page = await ctx.puppeteer.page();
+            //proxy setting
+            if (config.proxy) await useProxy(page, config.proxy === true ? ctx.root.config.request.proxyAgent : config.proxy)
             if (lock) await lock;
             let resolve: () => void;
             lock = new Promise((r) => { resolve = r; });
             try {
-                await page.goto(`https://enka.network/u/${session.user.genshin_uid}/`, {
+                await page.goto(`${config.agent}/u/${session.user.genshin_uid}/`, {
                     waitUntil: 'networkidle0',
                     timeout: 60000,
                 });
@@ -117,7 +127,7 @@ export function apply(ctx: Context, config: Config) {
                     await Promise.all([
                         page.waitForNetworkIdle({ idleTime: 100 }),
                         page.evaluate((inText) => {
-                            const input = document.querySelector(`[placeholder="${inText || 'Custom text'}"]`) as HTMLInputElement;
+                            const input = document.querySelector<HTMLInputElement>(`[placeholder="${inText || 'Custom text'}"]`);
                             input.value = 'Koishi & Enka Network';
                             input.dispatchEvent(new Event('input'));
                         }, userLang[1]),
@@ -125,13 +135,13 @@ export function apply(ctx: Context, config: Config) {
                     await page.click('button[data-icon="image"]');
                     const buf = await new Promise<Buffer>((resolve) => {
                         const cb = async (ev: HTTPResponse) => {
-                            if (!UUID.test(ev.request().url().trim())) return;
+                            if (!UUIDRegExp.test(ev.request().url().trim())) return;
                             page.off('response', cb);
                             resolve(await ev.buffer());
                         };
                         page.on('response', cb);
                     });
-                    if (cache && config.maxAge >= 30000) await cache.set(cacheKey, buf.toString('base64'), config.maxAge);
+                    console.log(buf)
                     return h.image(buf, 'image/png');
                 } else {
                     let msg = `<p>${session.text('.list')}</p>`
@@ -139,6 +149,7 @@ export function apply(ctx: Context, config: Config) {
                         list.forEach(character => {
                             if (character.includes('Costume'))
                                 character = character.split('Costume')[0]
+                            if ((session.user.locale === 'zh' || !session.user.locale) && map[character.toLowerCase()]) character = map[character.toLowerCase()].cnName
                             msg += `<p>${character}</p>`
                         })
                     } else {
